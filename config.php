53c2fe617d5072a8491298c9f13b5f82ab3c0404
chacachien <nguyentienphatbl123@gmail.com>
2024-09-25 15:44:49 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php  // Moodle configuration file
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'postgres';
+$CFG->dbpass    = '1307x2Npk';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+
+
+$CFG->wwwroot   = 'http://localhost:8080/moodle';
+$CFG->dataroot  = '/opt/homebrew/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
+ ini_set ('display_errors', 'on');
+ ini_set ('log_errors', 'on');
+ ini_set ('display_startup_errors', 'on');
+ ini_set ('error_reporting', E_ALL);
+ ini_set ('error_log', '/opt/homebrew/var/log/php_errors.log');
+ $CFG->debug = 30719;
---
+++ theme/boost/amd/src/widget.js
@@ -0,0 +1,11 @@
+define(["jquery", "theme_boost/widgetjs"], function ($, widgetjs) {
+  return {
+    init: function (data) {
+      console.log("data", data["userId"]);
+      console.log("widgetjs1", widgetjs);
+      widgetjs.default({
+        // botSubTitle: data["userId"],
+      });
+    },
+  };
+});
